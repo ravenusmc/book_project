a37d1a355c900b83e0c3d0db0245c53550cddbba
ravenusmc <mcuddy77@gmail.com>
2017-07-13 13:00:49 -0400

    set up system to make a homepage

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+//Requiring all of the packages
 const express = require('express');
+const path = require('path');
 
+//Setting up the constant variable which will be set equal to express.
 const app = express();
 
-//Start Server
+//loading view engine 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//Setting up public folder 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Home Page'
+  });
+});
+
+//Code to start the server.
 app.listen(3000, function(){
   console.log('Server Started on port 3000...')
 });
